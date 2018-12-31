e3e8d1bec06ad3490dfe44d562ffd59a19fc5a52
Giovanny B <giovannybm@gmail.com>
2018-12-30 23:16:33 -0500

    remix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  baseUrl: '/betamaster'
+  baseUrl: '/betamaster',
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "org.simulatedgreg.electron-vue",
+        linux: {
+          category: "Development",
+          target: [
+            "zip",
+            "AppImage"
+          ]
+        },
+        mac: {
+          target: [
+            "zip",
+            "dmg"
+          ]
+        },
+        win: {
+          target: [
+            "zip",
+            "nsis"
+          ]
+        }
+      }
+    }
+  }
 }
